0e559851e7dab765c0d4c73dfb13ead32e7b69bd
HiroIshida <spitfire.docomo@gmail.com>
2022-12-11 14:30:04 +0900

    refactor: use SimpleSetup

--- wrapper.cpp
+++ wrapper.cpp
@@ -3,10 +3,11 @@
 #include <ompl/base/spaces/RealVectorBounds.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/config.h>
+#include <ompl/geometric/SimpleSetup.h>
 #include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
+#include <ompl/tools/experience/ExperienceSetup.h>
 #include <ompl/tools/lightning/Lightning.h>
-#include <ompl/tools/thunder/Thunder.h>
 #include <ompl/util/PPM.h>
 #include <pybind11/functional.h>
 #include <pybind11/pybind11.h>
@@ -27,37 +28,34 @@ namespace ot = ompl::tools;
 
 namespace py = pybind11;
 
-ob::SpaceInformationPtr create_space_information(
-    const std::vector<double>& lb, const std::vector<double>& ub) {
-  const size_t dim = lb.size();
-  auto bounds = ob::RealVectorBounds(dim);
-  bounds.low = lb;
-  bounds.high = ub;
-  const auto space(std::make_shared<ob::RealVectorStateSpace>(dim));
-  space->setBounds(bounds);
-  const auto info = std::make_shared<ob::SpaceInformation>(space);
-  return info;
-}
-
 class OMPLConnectPlanner {
  public:
   OMPLConnectPlanner(const std::vector<double>& lb,
                      const std::vector<double>& ub,
                      std::function<bool(std::vector<double>)> is_valid,
                      size_t max_is_valid_call)
-      : planner_(nullptr),
+      : setup_(nullptr),
         is_valid_(is_valid),
         is_valid_call_count_(0),
         max_is_valid_call_(max_is_valid_call) {
-    const auto si = create_space_information(lb, ub);
-    planner_ = std::make_unique<og::RRTConnect>(si);
-    si->setStateValidityChecker(
+    // determine space
+    const size_t dim = lb.size();
+    auto bounds = ob::RealVectorBounds(dim);
+    bounds.low = lb;
+    bounds.high = ub;
+    const auto space(std::make_shared<ob::RealVectorStateSpace>(dim));
+    space->setBounds(bounds);
+    setup_ = std::make_unique<og::SimpleSetup>(space);
+    setup_->setStateValidityChecker(
         [this](const ob::State* s) { return this->is_valid(s); });
+    space->setup();
+    setup_->setPlanner(
+        std::make_shared<og::RRTConnect>(setup_->getSpaceInformation()));
   }
 
   std::optional<std::vector<std::vector<double>>> solve(
       const std::vector<double>& start, const std::vector<double>& goal) {
-    const auto& space = planner_->getSpaceInformation()->getStateSpace();
+    const auto space = setup_->getStateSpace();
     ob::ScopedState<ob::RealVectorStateSpace> start_state(space),
         goal_state(space);
 
@@ -67,20 +65,14 @@ class OMPLConnectPlanner {
       start_state->values[i] = start[i];
       goal_state->values[i] = goal[i];
     }
-    const auto pdef(std::make_shared<ob::ProblemDefinition>(
-        planner_->getSpaceInformation()));
-    pdef->setStartAndGoalStates(start_state, goal_state);
-
-    planner_->setProblemDefinition(pdef);
-    planner_->setup();
+    setup_->setStartAndGoalStates(start_state, goal_state);
 
     std::function<bool()> fn = [this]() { return this->is_terminatable(); };
-    const auto result = this->planner_->solve(fn);
+    const auto result = setup_->solve(fn);
     if (not result) {
       return {};
     }
-    const auto p = pdef->getSolutionPath()->as<og::PathGeometric>();
-
+    const auto p = setup_->getSolutionPath().as<og::PathGeometric>();
     const auto& states = p->getStates();
     auto trajectory = std::vector<std::vector<double>>(
         states.size(), std::vector<double>(dim));
@@ -97,7 +89,7 @@ class OMPLConnectPlanner {
   bool is_terminatable() { return is_valid_call_count_ > max_is_valid_call_; }
 
   bool is_valid(const ob::State* state) {
-    const size_t dim = planner_->getSpaceInformation()->getStateDimension();
+    const size_t dim = setup_->getSpaceInformation()->getStateDimension();
     auto vec = std::vector<double>(dim);
     const auto& rs = state->as<ob::RealVectorStateSpace::StateType>();
     for (size_t i = 0; i < vec.size(); ++i) {
@@ -107,7 +99,7 @@ class OMPLConnectPlanner {
     return is_valid_(vec);
   }
 
-  std::unique_ptr<ob::Planner> planner_;
+  std::unique_ptr<og::SimpleSetup> setup_;
   std::function<bool(std::vector<double>)> is_valid_;
   size_t is_valid_call_count_;
   const size_t max_is_valid_call_;
