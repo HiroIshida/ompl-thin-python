4c680a30f5945a4ab76ec932546e967ff3e4ba69
HiroIshida <spitfire.docomo@gmail.com>
2022-12-12 10:03:12 +0900

    refactor: don't use class

--- example/rrt_connect.py
+++ example/rrt_connect.py
@@ -1,16 +1,14 @@
 import _omplpy
-import matplotlib.pyplot as plt
-import numpy as np
+#import matplotlib.pyplot as plt
+setup = _omplpy.SimpleSetup([0, 0], [1, 1], lambda x: True, 1000)
+_omplpy.set_algorithm(setup, "rrtconnect")
+_omplpy.solve(setup, [0.1, 0.1], [0.9, 0.9])
 
-def is_valid(vec):
-    return np.linalg.norm(np.array(vec) - np.ones(2) * 0.5) > 0.3
-
-
-planner = _omplpy.OMPLPlanner([0, 0], [1, 1], is_valid, 1000)
-points = planner.solve([0.1, 0.1], [0.9, 0.9])
-
-for i in range(len(points) - 1):
-    p0 = points[i]
-    p1 = points[i + 1]
-    plt.plot([p0[0], p1[0]], [p0[1], p1[1]])
-plt.show()
+#planner = _omplpy.OMPLPlanner([0, 0], [1, 1], is_valid, 1000)
+#points = planner.solve([0.1, 0.1], [0.9, 0.9])
+#
+#for i in range(len(points) - 1):
+#    p0 = points[i]
+#    p1 = points[i + 1]
+#    plt.plot([p0[0], p1[0]], [p0[1], p1[1]])
+#plt.show()
--- wrapper.cpp
+++ wrapper.cpp
@@ -1,5 +1,7 @@
 #include <ompl/base/PlannerTerminationCondition.h>
 #include <ompl/base/State.h>
+#include <ompl/base/StateSampler.h>
+#include <ompl/base/StateSpace.h>
 #include <ompl/base/spaces/RealVectorBounds.h>
 #include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/config.h>
@@ -18,97 +20,132 @@
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <stdexcept>
 #include <vector>
 
 #include "ompl/base/Planner.h"
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
-namespace ot = ompl::tools;
 
 namespace py = pybind11;
 
-class OMPLConnectPlanner {
- public:
-  OMPLConnectPlanner(const std::vector<double>& lb,
-                     const std::vector<double>& ub,
-                     std::function<bool(std::vector<double>)> is_valid,
-                     size_t max_is_valid_call)
+template <typename SetupT>
+struct SetupWrapper {
+  SetupWrapper(std::vector<double> lb,
+               std::vector<double> ub,
+               std::function<bool(std::vector<double>)> is_valid,
+               size_t max_is_valid_call)
+      : SetupWrapper(bound2space(lb, ub), is_valid, max_is_valid_call)
+  {
+  }
+
+  SetupWrapper(ob::StateSpacePtr space,
+               std::function<bool(std::vector<double>)> is_valid,
+               size_t max_is_valid_call)
       : setup_(nullptr),
         is_valid_(is_valid),
         is_valid_call_count_(0),
-        max_is_valid_call_(max_is_valid_call) {
-    // determine space
+        max_is_valid_call_(max_is_valid_call)
+  {
+    setup_ = std::make_unique<SetupT>(space);
+    setup_->setStateValidityChecker(
+        [this](const ob::State* s) { return this->is_valid(s); });
+  }
+
+  void resetCount() { this->is_valid_call_count_ = 0; }
+
+  static std::shared_ptr<ob::StateSpace> bound2space(
+      const std::vector<double>& lb, const std::vector<double>& ub)
+  {
     const size_t dim = lb.size();
     auto bounds = ob::RealVectorBounds(dim);
     bounds.low = lb;
     bounds.high = ub;
     const auto space(std::make_shared<ob::RealVectorStateSpace>(dim));
     space->setBounds(bounds);
-    setup_ = std::make_unique<og::SimpleSetup>(space);
-    setup_->setStateValidityChecker(
-        [this](const ob::State* s) { return this->is_valid(s); });
     space->setup();
-    setup_->setPlanner(
-        std::make_shared<og::RRTConnect>(setup_->getSpaceInformation()));
+    return space;
   }
 
-  std::optional<std::vector<std::vector<double>>> solve(
-      const std::vector<double>& start, const std::vector<double>& goal) {
-    const auto space = setup_->getStateSpace();
-    ob::ScopedState<ob::RealVectorStateSpace> start_state(space),
-        goal_state(space);
-
-    const size_t dim = start.size();
-
-    for (size_t i = 0; i < dim; ++i) {
-      start_state->values[i] = start[i];
-      goal_state->values[i] = goal[i];
-    }
-    setup_->setStartAndGoalStates(start_state, goal_state);
-
-    std::function<bool()> fn = [this]() { return this->is_terminatable(); };
-    const auto result = setup_->solve(fn);
-    if (not result) {
-      return {};
-    }
-    const auto p = setup_->getSolutionPath().as<og::PathGeometric>();
-    const auto& states = p->getStates();
-    auto trajectory = std::vector<std::vector<double>>(
-        states.size(), std::vector<double>(dim));
-    for (size_t i = 0; i < states.size(); ++i) {
-      const auto& rs = states[i]->as<ob::RealVectorStateSpace::StateType>();
-      for (size_t j = 0; j < dim; ++j) {
-        trajectory[i][j] = rs->values[j];
-      }
-    }
-    return trajectory;
+  bool is_terminatable() const
+  {
+    return is_valid_call_count_ > max_is_valid_call_;
   }
 
- private:
-  bool is_terminatable() { return is_valid_call_count_ > max_is_valid_call_; }
-
-  bool is_valid(const ob::State* state) {
-    const size_t dim = setup_->getSpaceInformation()->getStateDimension();
+  bool is_valid(const ob::State* state)
+  {
+    const size_t dim = this->setup_->getSpaceInformation()->getStateDimension();
     auto vec = std::vector<double>(dim);
     const auto& rs = state->as<ob::RealVectorStateSpace::StateType>();
     for (size_t i = 0; i < vec.size(); ++i) {
       vec[i] = rs->values[i];
     }
-    is_valid_call_count_++;
+    this->is_valid_call_count_++;
     return is_valid_(vec);
   }
 
-  std::unique_ptr<og::SimpleSetup> setup_;
+  std::unique_ptr<SetupT> setup_;
   std::function<bool(std::vector<double>)> is_valid_;
   size_t is_valid_call_count_;
   const size_t max_is_valid_call_;
 };
 
-PYBIND11_MODULE(_omplpy, m) {
+void setAlgorithm(const SetupWrapper<og::SimpleSetup>& sw,
+                  const std::string& algoname)
+{
+  // don't use enum for algorithm. Enum seem to cause memory-leak by pybind
+  if (algoname.compare("rrtconnect") == 0) {
+    const auto space_info = sw.setup_->getSpaceInformation();
+    sw.setup_->setPlanner(std::make_shared<og::RRTConnect>(space_info));
+  } else {
+    throw std::runtime_error("not supported");
+  }
+}
+
+std::optional<std::vector<std::vector<double>>> solve(
+    const SetupWrapper<og::SimpleSetup>& sw,
+    const std::vector<double>& start,
+    const std::vector<double>& goal)
+{
+  const auto space = sw.setup_->getStateSpace();
+  ob::ScopedState<ob::RealVectorStateSpace> start_state(space),
+      goal_state(space);
+
+  const size_t dim = start.size();
+
+  for (size_t i = 0; i < dim; ++i) {
+    start_state->values[i] = start[i];
+    goal_state->values[i] = goal[i];
+  }
+  sw.setup_->setStartAndGoalStates(start_state, goal_state);
+
+  std::function<bool()> fn = [&sw]() { return sw.is_terminatable(); };
+  const auto result = sw.setup_->solve(fn);
+  if (not result) {
+    return {};
+  }
+  const auto p = sw.setup_->getSolutionPath().as<og::PathGeometric>();
+  const auto& states = p->getStates();
+  auto trajectory =
+      std::vector<std::vector<double>>(states.size(), std::vector<double>(dim));
+  for (size_t i = 0; i < states.size(); ++i) {
+    const auto& rs = states[i]->as<ob::RealVectorStateSpace::StateType>();
+    for (size_t j = 0; j < dim; ++j) {
+      trajectory[i][j] = rs->values[j];
+    }
+  }
+  return trajectory;
+}
+
+PYBIND11_MODULE(_omplpy, m)
+{
   m.doc() = "unofficial ompl python wrapper";
-  py::class_<OMPLConnectPlanner>(m, "OMPLPlanner")
-      .def(py::init<const std::vector<double>&, const std::vector<double>&,
-                    std::function<bool(std::vector<double>)>, size_t>())
-      .def("solve", &OMPLConnectPlanner::solve);
+  py::class_<SetupWrapper<og::SimpleSetup>>(m, "SimpleSetup")
+      .def(py::init<std::vector<double>,
+                    std::vector<double>,
+                    std::function<bool(std::vector<double>)>,
+                    size_t>());
+  m.def("set_algorithm", setAlgorithm, "set aglrithm");
+  m.def("solve", solve, "solve");
 }
