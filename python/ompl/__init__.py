5af41393a820744745e97c30b2954cd2815a9cd7
HiroIshida <spitfire.docomo@gmail.com>
2022-12-13 23:54:26 +0900

    refactor: create abstract python class for ompl planners

--- python/ompl/__init__.py
+++ python/ompl/__init__.py
@@ -1,8 +1,10 @@
-from . import _omplpy
+from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Sequence, Any, Callable, Optional, List, Union
 import numpy as np
 from pathlib import Path
+
+from . import _omplpy
 
 VectorLike = Union[np.ndarray, Sequence[float]]
 PathLike = Union[Path, str]
@@ -34,7 +36,7 @@ class Algorithm(Enum):
     FMT = "FMT"
 
 
-class Planner:
+class _OMPLPlannerBase(ABC):
     """
     An additional higher layer wrapper.
     One important reason of this class is exposing type hinting
@@ -43,7 +45,8 @@ class Planner:
     _is_valid: Callable[[VectorLike], bool]
 
     def __init__(self, lb: VectorLike, ub: VectorLike, is_valid: Callable[[VectorLike], bool], n_max_is_valid: int, fraction: float, algo: Algorithm = Algorithm.RRTConnect):
-        self._planner = _omplpy._OMPLPlanner(lb, ub, is_valid, n_max_is_valid, fraction, algo.value)
+        planner_t = self.planner_type()
+        self._planner = planner_t(lb, ub, is_valid, n_max_is_valid, fraction, algo.value)
         self._is_valid = is_valid
 
     def solve(self, start: VectorLike, goal: VectorLike) -> Optional[List[np.ndarray]]:
@@ -56,28 +59,24 @@ class Planner:
             ret[i] = np.array(ret[i])
         return ret
 
+    @abstractmethod
+    def planner_type(self) -> Any:
+        ...
 
-class LightningPlanner:
+
+class Planner(_OMPLPlannerBase):
     """
     An additional higher layer wrapper.
     One important reason of this class is exposing type hinting
     """
-    _planner: Any
-    _is_valid: Callable[[VectorLike], bool]
+    def planner_type(self) -> Any:
+        return _omplpy._OMPLPlanner
 
-    def __init__(self, lb: VectorLike, ub: VectorLike, is_valid: Callable[[VectorLike], bool], n_max_is_valid: int, fraction: float, algo: Algorithm = Algorithm.RRTConnect):
-        self._planner = _omplpy._LightningPlanner(lb, ub, is_valid, n_max_is_valid, fraction, algo.value)
-        self._is_valid = is_valid
 
-    def solve(self, start: VectorLike, goal: VectorLike) -> Optional[List[np.ndarray]]:
-        assert self._is_valid(start)
-        assert self._is_valid(goal)
-        ret = self._planner.solve(start, goal)
-        if ret is None:
-            return ret
-        for i in range(len(ret)):
-            ret[i] = np.array(ret[i])
-        return ret
+class LightningPlanner(_OMPLPlannerBase):
+
+    def planner_type(self) -> Any:
+        return _omplpy._LightningPlanner
 
     def scratch(self) -> None:
         self._planner.scratch()
