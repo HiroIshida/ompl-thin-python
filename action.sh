1e4eb4dc25e1bd3a541c60bdd79154f97f9105bf
HiroIshida <spitfire.docomo@gmail.com>
2023-01-13 17:47:50 +0900

    chore: update action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,14 @@
-cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug ..
+frag="debug"
+if [ $frag = "debug" ]; then
+    if [ ! -d "build_debug" ]; then
+        mkdir "build_debug"
+    fi
+    cd build_debug
+elif [ $frag = "release" ]; then
+    if [ ! -d "build_release" ]; then
+        mkdir "build_release"
+    fi
+    cd build_release
+fi
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=$frag ..
 make -j4
