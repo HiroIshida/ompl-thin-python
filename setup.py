53d271809a36d712fd0f817d42ec4994eff82adf
HiroIshida <spitfire.docomo@gmail.com>
2023-04-23 04:49:31 +0900

    0.0.16.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name="ompl-thin",
-    version="0.0.16",
+    version="0.0.16.1",
     description="thin ompl python wrapper",
     author="Hirokazu Ishida",
     license="MIT",
