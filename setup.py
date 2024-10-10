15e0e54e3959835544980d10150b30cc9e03c716
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-10 13:30:24 +0900

    0.0.18

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ install_requires = [
 
 setup(
     name="ompl-thin",
-    version="0.0.17",
+    version="0.0.18",
     description="thin ompl python wrapper",
     author="Hirokazu Ishida",
     license="MIT",
